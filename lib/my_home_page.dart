f6ca6d6f9f7d7df2fc3edd7483d20fa9210581df
azicha <askakauchi@gmail.com>
2024-01-12 12:08:15 +0300

    Code refactoring

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,12 +1,77 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
-  
+
   @override
-  Widget build(BuildContext context) {
-   return Scaffold();
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double result = 0;
+  TextEditingController numbercontroller = TextEditingController();
+
+  void convert() {
+    result = double.parse(numbercontroller.text) * 159.64;
+    setState(() {});
   }
 
-
+  @override
+  Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 1.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(80)),
+    );
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'KES ${result.toStringAsFixed(2)}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextField(
+                controller: numbercontroller,
+                decoration: const InputDecoration(
+                  hintText: 'Enter Amount in Euro',
+                  hintStyle: TextStyle(color: Colors.black26),
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: convert,
+                style: const ButtonStyle(
+                  foregroundColor: MaterialStatePropertyAll(Colors.white),
+                  backgroundColor: MaterialStatePropertyAll(Colors.deepPurple),
+                  fixedSize: MaterialStatePropertyAll(
+                    Size(500, 60),
+                  ),
+                ),
+                child: const Text('Convert'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
